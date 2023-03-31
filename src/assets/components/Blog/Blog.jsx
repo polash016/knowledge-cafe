fb83974d3e0d4000f9a0a9020d6ee3e377573fc5
polash016 <robin.shrkr@gmail.com>
2023-03-31 12:29:19 +0600

    authors photo added

--- src/assets/components/Blog/Blog.jsx
+++ src/assets/components/Blog/Blog.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import slider from '../../../../public/slider-1.jpg'
-import photo1 from '../../../../public/carousel1.jpg'
 
 const Blog = (props) => {
-    const {name,description,published,time} = props.blog
+    const {name,description,published,time,profile} = props.blog
     return (
         <div>
             <div className="card w-[80%] bg-base-100 shadow-xl">
   <figure><img src={slider} alt="Shoes" /></figure>
-  <div className="flex items-center gap-4  pt-6">
-    <img className='rounded-full w-18 h-14' src={photo1} alt="" />
+  
+  <div className="flex justify-between items-center gap-4 pt-6">
+    <div className='flex'>
+    <img className='rounded-full w-18 h-14' src={profile} alt="" />
     <div>
-    <p className=''>{name}</p>
-    <p>{published}</p>
+    <p className='text-xl font-bold'>{name}</p>
+    <p className='text-base font-light'>{published}(04 Days Ago)</p>
     </div>
+    
+    </div>
+    <div className=''>
+        <p className=''>{time}Min Read</p>
+    </div>
+    </div>
+    <div>
+        <h3 className='text-start'>{description}</h3>
     </div>
     <div className="card-actions justify-end">
       <div className="badge badge-outline">Fashion</div>
